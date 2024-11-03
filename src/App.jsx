395197a4111866bbfb25508896aaca7caaecc09d
waelbaraka <pytsud@gmail.com>
2024-11-03 19:19:43 +0400

    asSA

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import JoinCommunity from './componets/ContactUs/JoinCommunity'
 import FAQ from './componets/FAQ/FAQ'
 import GuideNav from './componets/GuideNav/GuideNav'
 import HeroSection from './componets/HeroSection/HeroSection'
-import Section2 from './componets/Section2/Section2'
 import Section3 from './componets/Section3/Section3'
 import Section5 from './componets/Section5/Section5'
 function App() {
@@ -11,7 +10,6 @@ function App() {
   return (
     <>
     <HeroSection />
-    <Section2 />
     <Section3 />
     <GuideNav />
     <FAQ />
